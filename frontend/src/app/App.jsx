e52bc313caa7bfcfed8275b4f2934c8418226687
Palak Singh <palak.singh6122004@gmail.com>
2026-05-05 12:34:58 +0530

    Initial frontend setup with Vite + React + Tailwind

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  
+
+  return (
+  <main className="h-screen w-full bg-gray-900 flex gap-3">
+<aside className="h-screen w-1/5 bg-amber-50"></aside>
+  </main>
+  )
+}
+
+export default App
